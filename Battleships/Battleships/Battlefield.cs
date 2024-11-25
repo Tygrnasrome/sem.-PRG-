b4fb1ffec8fb1ec423001a21b895c7d8482b10f7
Tygrnasrome <pskop.k10@gmail.com>
2024-11-25 22:00:42 +0100

    Almost done, AI difficulty 2 missing

--- Battleships/Battleships/Battlefield.cs
+++ Battleships/Battleships/Battlefield.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.AccessControl;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -14,42 +15,53 @@ namespace Battleships
         public bool Active { get; set; }
         static bool Valid_tiles { get; set; }
         public Ship SelectedShip { get; set; }
-        static Ship[] Ships { get; set; }
-        public Battlefield(int size, int[] location)
+		public int SelectedShipNum { get; set; }
+        private Ship[] Ships { get; set; }
+		public bool[,] RevealedFields { get; set; }
+		public Battlefield(int size, int[] location)
         {
             Field = new char[size,size];
-            Size = size;
+			RevealedFields = new bool[size, size];
+			Size = size;
             Active = false;
             Valid_tiles = true;
             Location = location;
-            Ships = new Ship[6] {new(5, [0, 0]), new(4, [0, 0]), new(3, [0, 0]), new(2, [0, 0]), new(1, [0, 0]), new(1, [0, 0]) };
+            Ships = new Ship[5] {new(5, [0, 0]), new(4, [0, 0]), new(3, [0, 0]), new(3, [0, 0]), new(2, [0, 0])};
             SelectedShip = Ships[0];
-            // nastavim bitevni pole na prazdna
-            for (int i = 0; i < size; i++)
+			SelectedShipNum = 0;
+			// nastavim bitevni pole na prazdna
+			for (int i = 0; i < size; i++)
             {
                 for (int j = 0; j < size; j++)
                 {
                     Field[i, j] = ' ';
                 }
             }
-            Field[4, 4] = 's';
-        }
-        public void DrawBattlefield(int[] selectedTile)
+			for (int i = 0; i < size; i++)
+			{
+				for (int j = 0; j < size; j++)
+				{
+					RevealedFields[i, j] = false;
+				}
+			}
+		}
+        public void DrawBattlefield(string headline,bool selecting = true, bool revealed = true)
         {
+			SelectedShip.ValidPlacement = true;
             Console.SetCursorPosition(Location[0], Location[1]);
             Console.Write("┌");
-            for (int i = 0; i < Size * 2; i++)
+			Console.Write(headline);
+            for (int i = 0; i < Size * 2 - headline.Length; i++)
                 Console.Write("─");
             Console.Write("┐");
             Console.WriteLine();
-            for (var col = 0; col < Size; col++)
+            for (var y = 0; y < Size; y++)
             {
-                Console.SetCursorPosition(Location[0], Location[1] + col + 1);
+                Console.SetCursorPosition(Location[0], Location[1] + y + 1);
                 Console.Write("|");
-                for (var row = 0; row < Size; row++)
+                for (var x = 0; x < Size; x++)
                 {
-                    DrawField([col, row], selectedTile[0] == row && selectedTile[1] == col && Active);
-                    //DrawField([col, row], selectedTile[0] == row && selectedTile[1] == col && Active);
+                    DrawField([x, y], SelectedShip.IsShipField([x, y]) && selecting, revealed);
                 }
                 //Console.Write("|\n");
                 Console.Write("|\n");
@@ -61,43 +73,31 @@ namespace Battleships
             Console.Write("┘");
             Console.WriteLine();
         }
-        public void DrawField(int[] location, bool is_tmp)
+        public void DrawField(int[] location, bool is_tmp, bool revealed)
         {
-            if (is_tmp)
+			if (!revealed && !RevealedFields[location[0], location[1]])
+			{
+				Console.ForegroundColor = ConsoleColor.Gray;
+				Console.Write("░░");
+				Console.ResetColor();
+				return;
+			}
+			if (is_tmp)
                 switch (Field[location[0], location[1]])
                 {
                     default: // tmp tiles
-                        bool validPlaced = true; 
-                        for (int i = 0;i < SelectedShip.Size;i++)
-                            switch (SelectedShip.Rotation%4)
-                            {
-                                case 0:
-                                    if (!IsValidPlaced([location[0] + i, location[1]]))
-                                        validPlaced = false; break;
-                                case 1:
-                                    if (!IsValidPlaced([location[0], location[1] + i]))
-                                        validPlaced = false; break;
-                                case 2:
-                                    if (!IsValidPlaced([location[0] - i, location[1]]))
-                                        validPlaced = false; break;
-                                case 3:
-                                    if (!IsValidPlaced([location[0], location[1] - i]))
-                                        validPlaced = false; break;
-                            }
+						Valid_tiles = true;
+						if (!IsValidPlaced([location[0], location[1]]))
+							Valid_tiles = false;
 
-                        if (validPlaced)
-                            Console.ForegroundColor = ConsoleColor.Green;
-                        else
-                        {
-                            Console.ForegroundColor = ConsoleColor.Red;
-                            Valid_tiles = false;
-                        }
-                        break;
-
-                    case 's': // error tmp tiles - cant be placed
-                        Console.ForegroundColor = ConsoleColor.Red;
-                        Valid_tiles = false;
-                        break;
+						if (Valid_tiles)
+							Console.ForegroundColor = ConsoleColor.Green;
+						else
+						{
+							Console.ForegroundColor = ConsoleColor.Red;
+							SelectedShip.ValidPlacement = false;
+						}
+						break;
                 }
             else
                 switch (Field[location[0], location[1]])
@@ -109,18 +109,21 @@ namespace Battleships
                         Console.ForegroundColor = ConsoleColor.White;
                         break;
                     case 'd': // destroyed ship tiles
-                        Console.ForegroundColor = ConsoleColor.DarkGray;
+                        Console.ForegroundColor = ConsoleColor.DarkRed;
                         break;
                 }
+
             Console.Write("██");
             Console.ResetColor();
         }
         public bool IsValidPlaced(int[] location)
         {
-            if (location[0] != Size)
+			if (Field[location[0], location[1]] == 's')
+				return false;
+			if (location[0] != Size-1)
                 if (Field[location[0] + 1, location[1]] == 's')
                     return false;
-            if (location[1] != Size)
+            if (location[1] != Size-1)
                 if (Field[location[0], location[1] + 1] == 's')
                     return false;
             if (location[0] != 0)
@@ -131,6 +134,116 @@ namespace Battleships
                     return false;
             return true;
         }
+		public void PlaceSelectedShip()
+		{
+			if (SelectedShip.ValidPlacement == false)
+				return; 
 
-    }
+			for (int i = 0; i < SelectedShip.Size; i++)
+			{
+				Field[SelectedShip.ShipFields[i,0], SelectedShip.ShipFields[i, 1]] = 's';
+			}
+			for (int i = 0; i < Ships.Length; i++)
+			{
+				if (i == SelectedShipNum)
+				{
+					SelectedShip.Placed = true;
+					Ships[i] = SelectedShip;
+				}
+			}
+			SelectNextShip();
+		}
+		public void SelectNextShip()
+		{
+			for (int i = 0; i < Ships.Length; i++)
+			{
+				if (!Ships[i].Placed)
+				{
+					SelectedShip = Ships[i];
+					SelectedShipNum = i;
+					return;
+				}
+			}
+		}
+		public bool	AllShipsPlaced()
+		{
+
+			for (int i = 0; i < Ships.Length; i++)
+			{
+				if (Ships[i].Placed == false) 
+					return false;
+			}
+			return true;
+		}
+		public bool AllShipsDestroyed()
+		{
+			foreach (var field in Field)
+			{
+				if (field == 's')
+					return false;
+			}
+			return true;
+		}
+		public void PlaceAllShips()
+		{
+			int x, y, rotation;
+			Random rand = new Random();
+			while (!AllShipsPlaced())
+			{
+                for (int i = 0; i < Ships.Length; i++)
+                {
+					if (Ships[i].Placed)
+						continue;
+					rotation = rand.Next(2);
+					switch (rotation)
+					{
+						case 0:
+							x = rand.Next(Size - Ships[i].Size);
+							y = rand.Next(Size);
+							break;
+						case 1:
+							x = rand.Next(Size);
+							y = rand.Next(Size - Ships[i].Size);
+							break;
+						default:
+							x = 0;
+							y = 0;
+							break;
+					}
+					SelectedShipNum = i;
+					Ships[i].Location = [x, y];
+					Ships[i].Rotation = rotation;
+					SelectedShip = Ships[i];
+					SelectedShip.UpdateFields();
+					bool validPlaced = true;
+					for (int j = 0; j < SelectedShip.Size; j++)
+					{
+						if (!IsValidPlaced([SelectedShip.ShipFields[j, 0], SelectedShip.ShipFields[j, 1]]))
+							validPlaced = false;
+					}
+					if (validPlaced)
+					{
+						SelectedShip.ValidPlacement = true;
+						PlaceSelectedShip();
+					}
+
+				}
+			}
+		}
+		public void DestroyField(int[] location)
+		{
+			if (Field[location[0], location[1]] == 's')
+			{
+				Field[location[0], location[1]] = 'd';
+			}
+			RevealedFields[location[0], location[1]] = true;
+		}
+		public void DrawSelectedField(int[] location)
+		{
+			Console.SetCursorPosition(location[0]*2 + Location[0] + 1, location[1] + Location[1] + 1);
+			Console.ForegroundColor = ConsoleColor.Green;
+			Console.Write("██");
+			Console.ResetColor();
+		}
+	}
 }
--- Battleships/Battleships/Program.cs
+++ Battleships/Battleships/Program.cs
@@ -11,39 +11,85 @@ namespace Battleships
 
         static int[] SelectedTile = {0, 0};
         static bool Valid_placement = true;
-
+		static int AIDifficulty = 0;
         static void Main(string[] args)
         {
-            Init();
-            while (true)
+			while(true)
             {
-                Valid_placement = true;
-                Console.SetCursorPosition(0, 0); // Reset cursor for redraw, Console.Clear() causes too many rendering - slow
+				Init();
+				// placing ships
+				AIBf.PlaceAllShips();
+				while (!PlayerBf.AllShipsPlaced())
+				{
+					Valid_placement = true;
+					Console.SetCursorPosition(0, 0); // Reset cursor for redraw, Console.Clear() causes too many rendering - slow
+					PlaceRender();
+					PlayerInput();
+				}
+				Console.SetCursorPosition(0, 0); 
+				PlaceRender();
+				Console.SetCursorPosition(0, 0); 
 
-                Render();
-                PlayerInput();
-                
-            }
-            // game loop
-            while (true)
-            {
-                Console.Clear();
-                Render();
-                PlayerInput();
-            }
+				while (AIDifficulty == 0)
+				{
+					Console.WriteLine("Battleships\n");
+					Console.WriteLine("Zadej obtiznost AI protivnika (1-2)");
+					Console.WriteLine("1 - very easy");
+					Console.WriteLine("2 - normal\n");
+					switch (Console.ReadKey(intercept: true).Key)
+					{
+						case ConsoleKey.D1:
+							AIDifficulty = 1;
+							break;
+						case ConsoleKey.D2:
+							AIDifficulty = 2;
+							break;
+						default:
+							Console.WriteLine("Nespravny vstup");
+							break;
+					}
+					Console.SetCursorPosition(0, 0); 
+				}
+				Console.Clear();
+
+				// game loop
+				while (!PlayerBf.AllShipsDestroyed() && !AIBf.AllShipsDestroyed())
+				{
+					Console.SetCursorPosition(0, 0); 
+					GameRender();
+					PlayerGameInput();
+
+				}
+			}
         }
         static void Init()
         {
             Console.OutputEncoding = System.Text.Encoding.UTF8;
             PlayerBf.Active = true;
         }
-        static void Render()
+        static void PlaceRender()
         {
-            Console.WriteLine("Battleships");
+            Console.WriteLine("Battleships\n");
+			Console.WriteLine("Pomoci sipek, WSAD, enter zvolte rozmisteni lodi");
+			Console.WriteLine("Muzete pouzit autofill klavesou Tab");
 
-            PlayerBf.DrawBattlefield(SelectedTile);
-            AIBf.DrawBattlefield(SelectedTile);
+            PlayerBf.DrawBattlefield("Hrac");
+            AIBf.DrawBattlefield("AI opponent", false, true);
         }
+		static int[] playerBfLoc = PlayerBf.Location;
+		static void GameRender()
+		{
+			Console.WriteLine("Battleships\n");
+
+			Console.WriteLine("Pomoci sipek, WSAD, enter namirte a strelte");
+			PlayerBf.Location = playerBfLoc;
+			PlayerBf.DrawBattlefield("Hrac", false);
+			AIBf.DrawBattlefield("AI opponent", false, false);
+			AIBf.DrawSelectedField(SelectedTile);
+
+			PlayerBf.Location = [60, 8];
+			PlayerBf.DrawBattlefield("Pohled AI", false, false);
+		}
         
         static void PlayerInput()
         {
@@ -52,31 +98,91 @@ namespace Battleships
             switch (Console.ReadKey(intercept: true).Key)
             {
                 case ConsoleKey.UpArrow:
-                    //if (SelectedTile[1] != 0)
-                    //    SelectedTile[1] -= 1;
+					if (SelectedTile[1] != 0)
+						SelectedTile[1] -= 1;
                     PlayerBf.SelectedShip.MoveUp();
                     break;
                 case ConsoleKey.DownArrow:
-                    //if (SelectedTile[1] != PlayerBf.Size-1)
-                    //    SelectedTile[1] += 1;
+					if (SelectedTile[1] != PlayerBf.Size-1)
+					SelectedTile[1] += 1;
                     PlayerBf.SelectedShip.MoveDown(PlayerBf.Size);
                     break;
                 case ConsoleKey.LeftArrow:
-                    //if (SelectedTile[0] != 0)
-                    //    SelectedTile[0] -= 1;
-                    PlayerBf.SelectedShip.MoveLeft();
+					if (SelectedTile[0] != 0)
+						SelectedTile[0] -= 1;
+					PlayerBf.SelectedShip.MoveLeft();
                     break;
                 case ConsoleKey.RightArrow:
-                    //if (SelectedTile[0] != PlayerBf.Size - 1)
-                    //    SelectedTile[0] += 1;
+					if (SelectedTile[0] != PlayerBf.Size - 1)
+						SelectedTile[0] += 1;
                     PlayerBf.SelectedShip.MoveRight(PlayerBf.Size);
-
                     break;
                 case ConsoleKey.Enter:
-                    //if (valid_placement)
-                    //    playerBattlefield[SelectedTile[1], SelectedTile[0]] = 's';
+					if (Valid_placement)
+						PlayerBf.PlaceSelectedShip();
                     break;
-            }
+				case ConsoleKey.A:
+				case ConsoleKey.W:
+					PlayerBf.SelectedShip.Rotate(PlayerBf.Size);
+					break;
+				case ConsoleKey.D:
+				case ConsoleKey.S:
+					PlayerBf.SelectedShip.Rotate(PlayerBf.Size);
+					break;
+				case ConsoleKey.Tab:
+					PlayerBf.PlaceAllShips();
+					break;
+			}
         }
-    }
+	
+		static void PlayerGameInput()
+		{
+			switch (Console.ReadKey(intercept: true).Key)
+			{
+				case ConsoleKey.UpArrow:
+					if (SelectedTile[1] != 0)
+						SelectedTile[1] -= 1;
+					break;
+				case ConsoleKey.DownArrow:
+					if (SelectedTile[1] != PlayerBf.Size - 1)
+						SelectedTile[1] += 1;
+					break;
+				case ConsoleKey.LeftArrow:
+					if (SelectedTile[0] != 0)
+						SelectedTile[0] -= 1;
+					break;
+				case ConsoleKey.RightArrow:
+					if (SelectedTile[0] != PlayerBf.Size - 1)
+						SelectedTile[0] += 1;
+					break;
+				case ConsoleKey.Enter:
+					AIBf.DestroyField(SelectedTile);
+					AIGameInput();
+					break;
+
+			}
+		}
+		static Random rand = new Random();
+		static void AIGameInput()
+		{
+			int x, y;
+			if (AIDifficulty == 1)
+			{
+				x = rand.Next(PlayerBf.Size);
+				y = rand.Next(PlayerBf.Size);
+				while (PlayerBf.RevealedFields[x, y])
+				{
+					x = rand.Next(PlayerBf.Size);
+					y = rand.Next(PlayerBf.Size);
+				}
+
+				PlayerBf.DestroyField([x, y]);
+			}
+			if (AIDifficulty == 2)
+			{
+				// TODO
+			}
+
+		}
+	}
 }
